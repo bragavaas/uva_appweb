56f381010a994d378b4c0778ace4054997d24c83
Andre <bragavaas@gmail.com>
2023-11-30 22:29:16 -0300

    meu deus do ceu não aguento mais

--- src/includes/header.php
+++ src/includes/header.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="../assets/css/reset.css"> <!-- Link to your Reset CSS file -->
     <link rel="stylesheet" href="../assets/css/style.css"> <!-- Link to your CSS file -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta http-equiv="Cache-control" content="no-cache">
 </head>
 <body>
--- src/pages/index.php
+++ src/pages/index.php
@@ -20,40 +20,117 @@
                 <p>Navegue pela nossa lista crescente de iates particulares disponíveis para aluguel no Rio, com mais cidades em breve.</p>
             </div>
         </div>
-        <div class="row">
+        <div class="row homepage-featured-items">
             <div class="col-4">
                 <div class="featured-item">
                     <img src="../assets/img/featured-1.jpg" alt="Featured Item">
                     <div class="featured-item-details">
-                        <h3>Nome do Iate</h3>
-                        <p>Localização</p>
-                        <p>Preço</p>
-                        <a href="contact.php" class="btn btn-primary">Ver Mais</a>
+                        <div class="yacht-hourly-rate"><span><strong>$850/hour</strong></span></div>
+                        <h3 class="yacht-ad"> ENJOY 65 FOOT CARVER YACHT IN TORONTO</h3>
+                        <div class="inline yacht-type"><i class="fa-solid fa-house"></i><span> Cruiser / With Captain</span></div>
+                        <div class="inline yacht-location"><i class="fa-solid fa-location-dot"></i><span> Rio de Janeiro</span></div>
+                        <div class="inline yacht-location"><i class="fa-solid fa-people-group"></i><span><strong> Capacidade:</strong> 20 Pessoas</span></div>
                     </div>
                 </div>
             </div>
             <div class="col-4">
-                <div class="featured-item">
+            <div class="featured-item">
                     <img src="../assets/img/featured-2.jpg" alt="Featured Item">
                     <div class="featured-item-details">
-                        <h3>Nome do Iate</h3>
-                        <p>Localização</p>
-                        <p>Preço</p>
-                        <a href="contact.php" class="btn btn-primary">Ver Mais</a>
+                        <div class="yacht-hourly-rate"><span><strong>$550/hour</strong></span></div>
+                        <h3 class="yacht-ad"> Sea Ray 500 Party Boat in Toronto</h3>
+                        <div class="inline yacht-type"><i class="fa-solid fa-house"></i><span> Cruiser / With Captain</span></div>
+                        <div class="inline yacht-location"><i class="fa-solid fa-location-dot"></i><span> Rio de Janeiro</span></div>
+                        <div class="inline yacht-location"><i class="fa-solid fa-people-group"></i><span><strong> Capacidade:</strong> 15 Pessoas</span></div>
                     </div>
                 </div>
             </div>
             <div class="col-4">
-                <div class="featured-item">
+            <div class="featured-item">
                     <img src="../assets/img/featured-3.jpg" alt="Featured Item">
                     <div class="featured-item-details">
-                        <h3>Nome do Iate</h3>
-                        <p>Localização</p>
-                        <p>Preço</p>
-                        <a href="contact.php" class="btn btn-primary">Ver Mais</a>
+                        <div class="yacht-hourly-rate"><span><strong>$300/hour</strong></span></div>
+                        <h3 class="yacht-ad"> 27' Private Yacht Rental</h3>
+                        <div class="inline yacht-type"><i class="fa-solid fa-house"></i><span> Cruiser / With Captain</span></div>
+                        <div class="inline yacht-location"><i class="fa-solid fa-location-dot"></i><span> Rio de Janeiro</span></div>
+                        <div class="inline yacht-location"><i class="fa-solid fa-people-group"></i><span><strong> Capacidade:</strong> 8 Pessoas</span></div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+<section class="booking-footer">
+    <div class="container">
+    <div class="row">
+        <div class="col-4">
+            <div class="booking-form">
+                <h2>Reserve Já</h2>
+                <form>
+                    <div class="form-group">
+                        <label for="booking-location"><i class="fa-solid fa-location-dot"></i> Localização</label>
+                        <select class="form-control" id="booking-location">
+                            <option>Rio de Janeiro</option>
+                            <option>Angra dos Reis</option>
+                            <option>Paraty</option>
+                            <option>Ilha Grande</option>
+                            <option>Paquetá</option>
+                            <option>Búzios</option>
+                            <option>Cabo Frio</option>
+                            <option>Arraial do Cabo</option>
+                        </select>
+                        <label for="booking-date"><i class="fa-solid fa-calendar"></i> Data</label>
+                        <input type="date" id="booking-date" name="trip-start" value="2023-11-30" min="2023-11-30" max="2024-11-30" />
+                        <label for="guests"><i class="fa-solid fa-people-group"></i> Convidados</label>
+                        <select class="form-control" id="guests">
+                            <option>1 Pessoa</option>
+                            <option>2 Pessoas</option>
+                            <option>3 Pessoas</option>
+                            <option>4 Pessoas</option>
+                            <option>5 Pessoas</option>
+                            <option>6 Pessoas</option>
+                            <option>7 Pessoas</option>
+                            <option>8 Pessoas</option>
+                            <option>9 Pessoas</option>
+                            <option>10 Pessoas</option>
+                            <option>11 Pessoas</option>
+                            <option>12 Pessoas</option>
+                            <option>13 Pessoas</option>
+                            <option>14 Pessoas</option>
+                            <option>15 Pessoas</option>
+                            <option>16 Pessoas</option>
+                            <option>17 Pessoas</option>
+                            <option>18 Pessoas</option>
+                            <option>19 Pessoas</option>
+                            <option>20 Pessoas</option> 
+                        </select>
+                    </div>
+                    <button type="submit" class="submit-btn">Reservar</button>
+                </form>
+            </div>
+        </div>
+        <div class="col-4 booking-footer-info">
+            <h2>DAILY RENTALS BY YACHT DAY</h2>
+            <p>Yacht Day is a marketplace for boat owners and boating lovers to connect.  Yacht Day facilitates the transaction between both parties allowing quick and seamless access to boat rentals while providing a steady income flow to boat owners.</p>
+            <p>We turn boats profitable and an alternative to boat ownership for our renters who jump straight to the enjoyment and pleasures of boating.</p>
+            <p>Yacht Day currently operates rentals in Toronto Canada with expansion to other cities in the pipeline.</p>
+            <div class="booking-quote"><span>“The best way to enjoy a boat is to rent it”</span>  </div>      
+        </div>
+        <div class="col-4 booking-footer-links">
+            <h2>QUICK LINKS</h2>
+            <ul>
+                <li><a href="#"><i class="fa-solid fa-chevron-right"></i> Home</a></li>
+                <li><a href="#"><i class="fa-solid fa-chevron-right"></i> Aluguel</a></li>
+                <li><a href="#"><i class="fa-solid fa-chevron-right"></i> Sobre</a></li>
+                <li><a href="#"><i class="fa-solid fa-chevron-right"></i> Contato</a></li>
+                <li><a href="#"><i class="fa-solid fa-chevron-right"></i> Suporte</a></li>
+            </ul>
+            <hr>
+            <h2>CONTATO</h2>
+            <i class="fa-solid fa-phone">+55 21 997235420</i>
+            <br>
+            <i class="fa-solid fa-envelope">contato@yacht.com.br</i>
+        </div>
+    </div>    
+    </div>
+</section>
 
